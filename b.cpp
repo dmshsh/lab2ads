4297a427e81959bc4e7279254c22eaa1cf69fb3c
dmshsh <mrdeemush@gmail.com>
2025-09-21 17:43:14 +0500

    Update b.cpp

--- b.cpp
+++ b.cpp
@@ -5,18 +5,18 @@ using namespace std;
 int main() {
     int numberOfWords, shift;
     cin >> numberOfWords >> shift;
-    deque<string> dimashyesos;
+    deque<string> d;
     for(int i = 0; i < numberOfWords; i++){
         string a; cin >> a;
-        dimashyesos.push_back(a);
+        d.push_back(a);
     }
     for(int i = 0; i < shift; i++){
-        dimashyesos.push_back(dimashyesos.front());
-        dimashyesos.pop_front();
+        d.push_back(d.front());
+        d.pop_front();
     }
     for(int i = 0; i < numberOfWords; i++){
-        cout << dimashyesos.front() << " ";
-        dimashyesos.pop_front();
+        cout << d.front() << " ";
+        d.pop_front();
     }
     return 0;
 }
