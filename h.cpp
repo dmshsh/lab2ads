1db7310924d27685dfd997289c560981081c5780
dmshsh <mrdeemush@gmail.com>
2025-09-21 13:10:08 +0500

    Update h.cpp

--- h.cpp
+++ h.cpp
@@ -1,143 +1,167 @@
 #include <iostream>
-  
 using namespace std;
 
 struct Node{
-	Node * prev;
-	Node * next;
-	string val;
-	Node(string _val){
-		prev = NULL;
-		next = NULL;
-		val = _val;
-	}	
+    int val;
+    Node* next;
+    Node(): val(0), next(nullptr) {}
+    Node(int x): val(x), next(nullptr) {}
+    Node(Node* next): val(0), next(next) {}
+    Node(int x, Node* next): val(x), next(next) {}
 };
 
-Node * head = NULL;
-Node * tail = NULL;
-int cnt;
-void add_back(string s){
-    Node *node=new Node(s);
-    if (tail==NULL){
-        tail=node;
-        head=node;
+Node* insert(Node* head, Node* node, int p){
+    if (!node) return head;
+    if (p == 0) {
+        node->next = head;
+        return node;
     }
-    else{
-        node->prev=tail;
-        tail->next=node;
-        tail=node;
-    }
-}
-void add_front(string s){
-        Node *node = new Node(s);
-        if (head == NULL) {
-            tail = node;
-            head = node;
-        } else {
-           head->prev = node;
-            node->next =head;
-            head = node;
-        }
-    }
-bool empty(){
-    if (head==NULL && tail==NULL) return true;
-    else return false;
-}
-void erase_front(){
-    if (head!=NULL){
-		head=head->next;
-		if (head!=NULL)
-			head->prev=NULL;
-		else
-			tail=NULL;
-    }
-}
-void erase_back(){
-    if (tail!=NULL){
-        tail=tail->prev;
-        if (tail!=NULL)
-            tail->prev=NULL;
-        else
-            head=NULL;
-    }
-}
-string front(){
-	return head->val;
-}
-string back(){
-    return tail->val;
-}
-void clear(){
     Node* cur = head;
-    while (cur){
-        Node* nxt = cur->next;
-        delete cur;
-        cur = nxt;
+    for (int i = 0; i < p - 1 && cur; ++i) cur = cur->next;
+
+    if (cur) {
+        node->next = cur->next;
+        cur->next  = node;
     }
-    head = tail = NULL;
+    return head;
 }
 
+Node* remove(Node* head, int p){
+    if (!head) return head;
+    if (p == 0) {
+        Node* nxt = head->next;
+        delete head;
+        return nxt;
+    }
+    Node* cur = head;
+    for (int i = 0; i < p - 1 && cur; ++i) cur = cur->next;
+    if (cur && cur->next) {
+        Node* del = cur->next;
+        cur->next = del->next;
+        delete del;
+    }
+    return head;
+}
+
+Node* replace(Node* head, int p1, int p2){
+   
+    if (!head || p1 == p2) return head;
+
+    
+    Node* moved = nullptr;
+    if (p1 == 0) {
+        moved = head;
+        head  = head->next;
+    } else {
+        Node* cur = head;
+        for (int i = 0; i < p1 - 1 && cur; ++i) cur = cur->next;
+        moved = cur->next;             
+        cur->next = moved->next;
+    }
+    moved->next = nullptr;
+
   
-int main()
-{
-	string s;
-   	while(cin >> s){
-   		if(s == "add_front"){
-   			string t;
-   			cin >> t;
-   			add_front(t);
-   			cout << "ok" << endl;
-   		}
-   		if(s == "add_back"){
-   			string t;
-   			cin >> t;
-   			add_back(t);
-   			cout << "ok" << endl;
-   		}
-   		if(s == "erase_front"){
-   			if(empty()){
-   				cout << "error" << endl;
-   			}
-   			else
-   			{
-   				cout << front() << endl;
-   				erase_front();
-   			}
-   		}
-   		if(s == "erase_back"){
-   			if(empty()){
-   				cout << "error" << endl;
-   			}
-   			else{
-   				cout << back() << endl;
-   				erase_back();
-   			}
-   		}
-   		if(s == "front"){
-   			if(empty()){
-   				cout << "error" << endl;
-   			}
-   			else{
-   				cout << front() << endl;
-   			}
-   		}
-   		if(s == "back"){
-   			if(empty()){
-   				cout << "error" << endl;
-   			}
-   			else{
-   				cout << back() << endl;
-   			}
-   		}
-   		if(s == "clear"){
-   			clear();
-   			cout << "ok" << endl;
-   		}
-   		if(s == "exit"){
-   			cout << "goodbye" << endl;
-   			break;
-   		}
-   		// cout << "hi" << endl;
-   	}
+    if (p2 == 0) {
+        moved->next = head;
+        head = moved;
+        return head;
+    } else {
+        Node* cur = head;
+        for (int i = 0; i < p2 - 1 && cur; ++i) cur = cur->next;
+        moved->next = cur->next;
+        cur->next = moved;
+        return head;
+    }
+}
+
+Node* reverse(Node* head){
+    Node* prev = nullptr;
+    Node* cur  = head;
+    while (cur) {
+        Node* nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur  = nxt;
+    }
+    return prev;
+}
+
+void print(Node* head){
+    if (!head) { cout << -1 << "\n"; return; }
+    Node* cur = head;
+    bool first = true;
+    while (cur) {
+        if (!first) cout << ' ';
+        cout << cur->val;
+        first = false;
+        cur = cur->next;
+    }
+    cout << "\n";
+}
+
+Node* cyclic_left(Node* head, int x){
+    if (!head || !head->next || x == 0) return head;
+
+
+    int n = 1;
+    Node* tail = head;
+    while (tail->next) { tail = tail->next; ++n; }
+
+    int k = x % n;
+    if (k == 0) return head;
+
+    Node* cutPrev = head;
+    for (int i = 1; i < k; ++i) cutPrev = cutPrev->next; 
+    Node* newHead = cutPrev->next;
+
+    cutPrev->next = nullptr;
+    tail->next = head;
+
+    return newHead;
+}
+
+Node* cyclic_right(Node* head, int x){
+    if (!head || !head->next || x == 0) return head;
+
+
+    int n = 1;
+    Node* tail = head;
+    while (tail->next) { tail = tail->next; ++n; }
+
+    int k = x % n;
+    if (k == 0) return head;
+
+
+    return cyclic_left(head, n - k);
+}
+
+int main(){
+    Node* head = nullptr;
+    while (true)
+    {
+        int command; if(!(cin >> command)) break;
+        if (command == 0){
+            break;
+        }else if(command == 1){
+            int x, p; cin >> x >> p;
+            head = insert(head, new Node(x), p);
+        }else if (command == 2){
+            int p; cin >> p;
+            head = remove(head, p);
+        }else if (command == 3){
+            print(head);
+        }else if (command == 4){
+            int p1, p2; cin >> p1 >> p2;
+            head = replace(head, p1, p2);
+        }else if (command == 5){
+            head = reverse(head);
+        }else if (command == 6){
+            int x; cin >> x;
+            head = cyclic_left(head, x);
+        }else if (command == 7){
+            int x; cin >> x;
+            head = cyclic_right(head, x);
+        }   
+    }
     return 0;
 }
